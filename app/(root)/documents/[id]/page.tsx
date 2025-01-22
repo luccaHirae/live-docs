257329f46d1c2933a5ce95a2e3a15e3e87d62425
Bruno Hirae <brunodeluca117@outlook.com>
2025-01-21 21:51:52 -0300

    feat: add clerk auth

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,13 +1,16 @@
 import { Editor } from '@/components/editor/Editor';
 import Header from '@/components/Header';
+import { UserButton } from '@clerk/nextjs';
 
 const Document = () => {
   return (
     <div>
       <Header>
         <div className='flex w-fit items-center justify-center gap-2'>
-          <p className='document-title'>This is a placeholder document title</p>
+          <p className='document-title'>Share</p>
         </div>
+
+        <UserButton />
       </Header>
       <Editor />
     </div>
