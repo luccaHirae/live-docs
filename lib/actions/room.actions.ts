098bd92c33d9aa7e904cc197c7f86aa43db7f112
Bruno Hirae <brunodeluca117@outlook.com>
2025-01-24 10:44:22 -0300

    feat: add update document server action

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -57,3 +57,25 @@ export const getDocument = async ({
     console.log(`Error getting document: ${error}`);
   }
 };
+
+export const updateDocument = async ({
+  roomId,
+  title,
+}: {
+  roomId: string;
+  title: string;
+}) => {
+  try {
+    const updatedRoom = await liveblocks.updateRoom(roomId, {
+      metadata: {
+        title,
+      },
+    });
+
+    revalidatePath(`/documents/${roomId}`);
+
+    return parseStringify(updatedRoom);
+  } catch (error) {
+    console.log(`Error updating document: ${error}`);
+  }
+};
