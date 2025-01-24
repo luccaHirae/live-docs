7c55f872cd6244ec6f0f6cd90a764f419702adb1
Bruno Hirae <brunodeluca117@outlook.com>
2025-01-24 14:04:59 -0300

    feat: enable user mentions

--- app/provider.tsx
+++ app/provider.tsx
@@ -2,9 +2,14 @@
 
 import React from 'react';
 import { LiveblocksProvider } from '@liveblocks/react/suspense';
-import { getClerkUsers } from '@/lib/actions/user.actions';
+import { getClerkUsers, getDocumentUsers } from '@/lib/actions/user.actions';
+import { useUser } from '@clerk/nextjs';
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
+  const { user } = useUser();
+
+  if (!user) return null;
+
   return (
     <LiveblocksProvider
       authEndpoint={'/api/liveblocks-auth'}
@@ -12,6 +17,15 @@ const Provider = ({ children }: { children: React.ReactNode }) => {
         const users = await getClerkUsers({ userIds });
 
         return users;
+      }}
+      resolveMentionSuggestions={async ({ text, roomId }) => {
+        const roomUsers = await getDocumentUsers({
+          roomId,
+          currentUser: user?.emailAddresses[0].emailAddress,
+          text,
+        });
+
+        return roomUsers;
       }}
     >
       {children}
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,27 +1,62 @@
-'use server'
+'use server';
 
-import { clerkClient } from "@clerk/nextjs/server";
-import { parseStringify } from "@/lib/utils";
+import { clerkClient } from '@clerk/nextjs/server';
+import { parseStringify } from '@/lib/utils';
+import { liveblocks } from '@/lib/liveblocks';
 
 export const getClerkUsers = async ({ userIds }: { userIds: string[] }) => {
   try {
     const clerk = await clerkClient();
 
     const { data } = await clerk.users.getUserList({
-      emailAddress: userIds
+      emailAddress: userIds,
     });
 
     const users = data.map((user) => ({
       id: user.id,
       name: `${user.firstName} ${user.lastName}`,
       email: user.emailAddresses[0].emailAddress,
-      avatar: user.imageUrl
+      avatar: user.imageUrl,
     }));
 
-    const sortedUsers = userIds.map((email) => users.find((user) => user.email === email));
+    const sortedUsers = userIds.map((email) =>
+      users.find((user) => user.email === email)
+    );
 
     return parseStringify(sortedUsers);
   } catch (error) {
     console.log(`Error getting users: ${error}`);
   }
-}
+};
+
+export const getDocumentUsers = async ({
+  roomId,
+  currentUser,
+  text,
+}: {
+  roomId: string;
+  currentUser: string;
+  text: string;
+}) => {
+  try {
+    const room = await liveblocks.getRoom(roomId);
+
+    const users = Object.keys(room.usersAccesses).filter(
+      (email) => email !== currentUser
+    );
+
+    if (text.length === 0) {
+      const lowerCaseText = text.toLowerCase();
+
+      const filteredUsers = users.filter((email) =>
+        email.toLowerCase().includes(lowerCaseText)
+      );
+
+      return parseStringify(filteredUsers);
+    }
+
+    return parseStringify(users);
+  } catch (error) {
+    console.log(`Error getting document users: ${error}`);
+  }
+};
